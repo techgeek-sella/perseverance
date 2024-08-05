4621c9887c7c90f98caa7c98561b5f82e89b2995
Selladurai-Vadivel <selladurai.vadivel@carrier.com>
2024-08-05 07:59:09 -0400

    cool

---
+++ App.js
@@ -0,0 +1,8 @@
+const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1","","I'm Sella")))
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+
+root.render(parent)
+
+
